29c95ee48a5a86b400b48d9cec14cc6501906c90
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-23 21:35:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/newsletter-sign-up/tags/1.3.1@376403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ newsletter-sign-up.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Newsletter Sign-Up
+Plugin URI: http://DannyvanKooten.com/wordpress-plugins/newsletter-sign-up/
+Description: Adds a checkbox to your comment form to turn your commenters into subscribers
+Version: 1.3.1
+Author: Danny van Kooten
+Author URI: http://DannyvanKooten.com
+License: GPL2
+*/
+
+/*  Copyright 2010  Danny van Kooten  (email : danny@vkimedia.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* 
+	TO DO
+	
+	* Add %%NAME%% to use in additional data
+	* Add %%IP%% to use in additional data
+	
+*/
+
+require_once('php/widget.php');
+require_once('php/frontend.php');
+$Newsletter_SignUp = new Newsletter_SignUp();
+
+if(is_admin()) {
+	require_once('php/backend.php');
+	$Newsletter_SignUp_Admin = new Newsletter_SignUp_Admin();
+}
+
+function ns_comment_checkbox()
+{
+	global $Newsletter_SignUp;
+	$Newsletter_SignUp->add_checkbox();
+}
