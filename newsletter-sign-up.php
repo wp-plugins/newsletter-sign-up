5d36f8fea59da3b93a67e76f35f34bfa5d19c3d7
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-17 19:35:57 +0000

    1.5.1 - mostly code refactoring
    git-svn-id: https://plugins.svn.wordpress.org/newsletter-sign-up/tags/1.5.1@464323 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ newsletter-sign-up.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Newsletter Sign-Up
+Plugin URI: http://DannyvanKooten.com/wordpress-plugins/newsletter-sign-up/
+Description: Adds various ways for your visitors to sign-up to your mailinglist (checkbox, widget, form)
+Version: 1.5.1
+Author: Danny van Kooten
+Author URI: http://DannyvanKooten.com
+License: GPL2
+*/
+
+/*  Copyright 2010  Danny van Kooten  (email : danny@vkimedia.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* 
+	TO DO
+	
+	* Add %%NAME%% to use in additional data
+	* Add %%IP%% to use in additional data
+	
+*/
+
+require_once('frontend/NewsletterSignUpWidget.php');
+require_once('frontend/NewsletterSignUp.php');
+
+$NewsletterSignUp = NewsletterSignUp::getInstance();
+
+if(is_admin()) {
+	require_once('backend/NewsletterSignUpAdmin.php');
+	$NewsletterSignUpAdmin = new NewsletterSignUpAdmin();
+}
+
+/**
+* Displays the comment checkbox, call this function if your theme does not use the 'comment_form' action in the comments.php template.
+*/
+function ns_comment_checkbox()
+{
+	$NewsletterSignUp = NewsletterSignUp::getInstance();
+	$NewsletterSignUp->add_checkbox();
+}
+
+/**
+* Outputs a sign-up form, for usage in your theme files.
+*/
+function nsu_signup_form()
+{
+	$NewsletterSignUp = NewsletterSignUp::getInstance();
+	$NewsletterSignUp->output_form(true);
+}
